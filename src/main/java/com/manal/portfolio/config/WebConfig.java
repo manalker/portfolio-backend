85b999ba63cfe8a1fb77adddfb7cd6f42f0183ad
Manal <manal.kerroumi@gmail.com>
2026-05-30 22:09:37 +0100

    Add CORS config for Vercel

--- src/main/java/com/manal/portfolio/config/WebConfig.java
+++ src/main/java/com/manal/portfolio/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.manal.portfolio.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,12 +9,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "https://portfolio-frontend-gold.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/uploads/certifications/**")
                 .addResourceLocations("file:src/main/resources/static/certifications/");
 
-        // Images (si tu en as dans uploads/images)
         registry.addResourceHandler("/uploads/images/**")
                 .addResourceLocations("classpath:/static/uploads/images/");
     }
